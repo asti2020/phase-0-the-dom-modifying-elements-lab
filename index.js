40b2c9f70efc7cf89ce36dfab95332d96c880d7a
Astika Luintel <astild2020@gmail.com>
2022-09-01 19:23:53 -0400

    commit tree 4157451d07074fb73decaf89f31f0d7100a01aa3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.getElementById("main");
+if (main.parentNode) {
+  main.parentNode.removeChild(main);
+}
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "My name is the champion"
